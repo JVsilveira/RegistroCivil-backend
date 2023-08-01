11dd2c7d1b3c93cc18a246c5f1b0c46cf005245f
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-08-01 15:00:30 -0300

    .

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const app = require('express')()
-const consign = require('consign')
-const db = require('./config/db')
+const app = require("express")()
+const consign = require("consign")
+const db = require("./config/db")
 app.db = db
+const cors = require("cors")
+
+app.use(
+  cors({
+    origin: "https://registro-civil-frontend.vercel.app",
+  })
+)
 
 consign()
-  .then('./config/middlewares.js')
-  .then('./api/validator.js')
-  .then('./api/nascimento.js')
-  .then('./api/casamento.js')
-  .then('./api/obito.js')
-  .then('./api/user.js')
-  .then('./api/auth.js')
-  .then('./config/routes.js')
+  .then("./config/middlewares.js")
+  .then("./api/validator.js")
+  .then("./api/nascimento.js")
+  .then("./api/casamento.js")
+  .then("./api/obito.js")
+  .then("./api/user.js")
+  .then("./api/auth.js")
+  .then("./config/routes.js")
   .into(app)
 
 app.listen(3000, () => {
-  console.log('Backend executando...')
+  console.log("Backend executando...")
 })
