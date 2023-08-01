c959f441c06b9f435e4b0548671406d6285baa54
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-08-01 15:22:44 -0300

    .

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,7 @@
 module.exports = {
   client: "postgresql",
-  connection: {
-    host: "ep-sparkling-field-44311282.us-east-2.aws.neon.tech",
-    port: 5432,
-    database: "neondb",
-    user: "JVsilveira",
-    password: "nNHupZ4sE5vM",
-    ssl: true,
-  },
+  connection:
+    "postgres://JVsilveira:nNHupZ4sE5vM@ep-sparkling-field-44311282.us-east-2.aws.neon.tech/neondb",
   migrations: {
     tableName: "knex_migrations",
   },
