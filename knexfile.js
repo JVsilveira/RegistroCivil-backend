c698ea6193c4d167b4db054d14339d2259ab6198
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-08-01 15:28:05 -0300

    .

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,13 @@
 module.exports = {
   client: "postgresql",
-  connection:
-    "postgres://JVsilveira:nNHupZ4sE5vM@ep-sparkling-field-44311282.us-east-2.aws.neon.tech/neondb",
+  connection: {
+    PGHOST: "ep-sparkling-field-44311282.us-east-2.aws.neon.tech",
+    PGDATABASE: "neondb",
+    PGUSER: "JVsilveira",
+    PGPASSWORD: "nNHupZ4sE5vM",
+    ENDPOINT_ID: "ep-sparkling-field-44311282",
+  },
+
   migrations: {
     tableName: "knex_migrations",
   },
