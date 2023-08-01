c096801103e963210bbfa4940c02e2dfe5bd9e6d
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-08-01 14:14:35 -0300

    .

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = app => {
   app.use(bodyParser.json())
   app.use(
     cors({
-      origin: "https://registro-civil-frontend.vercel.app",
+      origin: "https://registro-civil-frontend-p343jm7z2-jvsilveira.vercel.app",
     })
   )
 }
