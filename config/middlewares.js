85f6e18f26ab98ef147924473036f86258e7ab15
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-08-01 14:11:39 -0300

    .

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,5 +3,9 @@ const cors = require("cors")
 
 module.exports = app => {
   app.use(bodyParser.json())
-  app.use(cors("https://registro-civil-frontend.vercel.app"))
+  app.use(
+    cors({
+      origin: "https://registro-civil-frontend.vercel.app",
+    })
+  )
 }
