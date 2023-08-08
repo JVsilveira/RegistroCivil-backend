d3194e05d3693906aea180e24e4932e6657bb46f
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-08-08 11:04:17 -0300

    .

--- api/validator.js
+++ api/validator.js
@@ -1,8 +1,8 @@
 module.exports = app => {
   function existsOrError(value, message) {
-    if (!value) throw message
-    if (Array.isArray(value) && value.lenght === 0) throw message
-    if (typeof value === "string" && !value.trim()) throw message
+    if (!value) throw new Error(message)
+    if (Array.isArray(value) && value.lenght === 0) throw new Error(message)
+    if (typeof value === "string" && !value.trim()) throw new Error(message)
   }
 
   function notExistsOrError(value, message) {
@@ -11,11 +11,11 @@ module.exports = app => {
     } catch (message) {
       return
     }
-    throw message
+    throw new Error(message)
   }
 
   function equalsOrError(valueA, valueB, message) {
-    if (valueA !== valueB) throw message
+    if (valueA !== valueB) new Error(message)
   }
   return { equalsOrError, existsOrError, notExistsOrError }
 }
